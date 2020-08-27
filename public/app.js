beebfcd7ad01347e96f96a0f00cd7b9d9dec0a35
Devin Castro <dcrocker21@gmail.com>
2020-08-27 00:26:03 -0700

    FULLY FUNTIONAL

--- public/app.js
+++ public/app.js
@@ -12,12 +12,15 @@ document.getElementById('submit').addEventListener('click', event => {
       let burgerElem = document.createElement('li')
       burgerElem.id = data.id
       burgerElem.className = 'list-group-item list-group-item-primary'
+      burgerElem.dataset.name = document.getElementById('burger').value
       burgerElem.innerHTML = `
         Burger Name: ${document.getElementById('burger').value}
         <button class="btn btn-success devour">✓</button>
       `
       // now append the burger list element to the not devourded unordered list 
       document.getElementById('notDevoured').append(burgerElem)
+      // clear out the input
+      document.getElementById('burger').value = ''
     })
     .catch(err => console.log(err))
 })
@@ -27,7 +30,25 @@ document.addEventListener('click', event => {
   event.preventDefault()
   if (event.target.classList.contains('devour')) {
     console.log('ping')
-  } else if (event.target.classList.contains('delete')) {
-    console.log('ginp')
+    axios.put(`api/burgers/${event.target.parentNode.id}`, {
+      devoured: true
+    })
+    .then(({data}) => {
+      console.log(({data}))
+      // create a new list element for the newly devoured burger
+      let burgerElem = document.createElement('li')
+      burgerElem.id = data.id
+      burgerElem.className = 'list-group-item list-group-item-success'
+      burgerElem.innerHTML = `
+      Burger Name: ${event.target.parentNode.dataset.name}
+      `
+      // now append the burger list element to the not devourded unordered list 
+      document.getElementById('devoured').append(burgerElem)
+      
+      // then remove the entire list element from the not devoured list
+      event.target.parentNode.remove()
+      
+    })
+    .catch(err => console.log(err))
   }
 })
