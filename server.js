c32f15b3dd7da4cabbe54135193774f98d949105
Devin Castro <dcrocker21@gmail.com>
2020-08-26 21:38:29 -0700

    BROUGHT IN PACKAGES AND SET UP SERVER

---
+++ server.js
@@ -0,0 +1,20 @@
+// bring in these packages
+const express = require('express')
+const { join } = require('path')
+
+// create and express server
+const app = express()
+
+// bring in our middleware
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// bring in handlebars and change the extension to .hbs
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+// app.use(require('./controllers'))
+
+// let the app listen on a port
+app.listen(process.env.PORT || 3000)
